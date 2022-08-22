b7f62c8911008d8a3d0ef31811257986e5d3c721
Vladislav <1@zithen.com>
2022-08-23 00:31:41 +0300

    Add HW solution module3 task5
    Get min and max values from string into object

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let minMaxObj = {};
+
+  let itemArr = str
+                .split(' ')
+                .map(item => +item)
+                .filter(item => !isNaN(item))
+                .sort( (a, b) => (a - b) )
+
+  return minMaxObj = {
+                      min: itemArr[0],
+                      max: itemArr.at(-1)
+                    };
 }
